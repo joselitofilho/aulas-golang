7ecaf05fa6d9272e2491c83b2d73107c57062f7b
Joselito Viveiros Nogueira Filho <joselito.viveiros@hp.com>
2021-07-02 11:04:10 -0300

    updates aula01.

--- aula01/main.go
+++ aula01/main.go
@@ -10,17 +10,53 @@ func main() {
 	num := 5
 	if num == 0 {
 		fmt.Println("1")
-		return
 	} else {
 		var result int = 1
 		for i := 1; i <= num; i++ {
 			result = result * i
 		}
 		fmt.Println(result)
-		return
 	}
 
 	// fibb(0) = 0
 	// fibb(1) = 1
 	// fibb(n) = fibb(n-1) + fibb(n-2) - if n > 1
+	var a, b, c uint
+	a = 0
+	b = 1
+	i := 2
+	num = 6
+	if num == 0 {
+		fmt.Println(b)
+	} else if num == 1 {
+		fmt.Println(b)
+	} else {
+		if num == 2 {
+			fmt.Println(b)
+		} else {
+			for i <= num {
+				c = a + b
+				a = b
+				b = c
+				i++
+			}
+			fmt.Println(c)
+		}
+	}
+
+	// switch
+	expression := 10
+	switch expression {
+	case 1:
+		fmt.Println(1)
+	case 2:
+		fmt.Println(2)
+	case 10:
+		fmt.Println(10)
+		fmt.Println(10)
+		fmt.Println(10)
+		fmt.Println(10)
+	default:
+		fmt.Println("default")
+	}
 }
